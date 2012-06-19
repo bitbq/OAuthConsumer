1bdf610697f78e3456c58f12933da8904d5ac393
Nathaniel Griswold <nategriswold@gmail.com>
2012-06-18 17:08:59 -0700

    Add iOS static lib

---
+++ OAuthConsumer iOS/OAuthConsumer_iOS.m
@@ -0,0 +1,13 @@
+//
+//  OAuthConsumer_iOS.m
+//  OAuthConsumer iOS
+//
+//  Created by Nathaniel Griswold on 6/18/12.
+//  Copyright (c) 2012 Nathaniel Griswold. All rights reserved.
+//
+
+#import "OAuthConsumer_iOS.h"
+
+@implementation OAuthConsumer_iOS
+
+@end
